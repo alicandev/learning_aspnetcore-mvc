ed9cc8a9628b17eadfe613fddd2963404eb25474
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-11 20:48:50 +0100

    Configuration using `appsettings.json` file.

--- ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +13,13 @@ namespace ExploreCalifornia
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+        
+        public Startup (IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -21,10 +29,10 @@ namespace ExploreCalifornia
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseExceptionHandler("/error.html");
-            
-            if (env.IsDevelopment())
+            if (_configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions"))
                 app.UseDeveloperExceptionPage();
+            else
+                app.UseExceptionHandler("/error.html");
 
             app.Use(async (ctx, next) =>
             {
