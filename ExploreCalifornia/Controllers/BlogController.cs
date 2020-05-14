e581ac49ade850326432d50691502ec2ad1f6642
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-14 09:07:34 +0100

    Blog controller and its actions edited to use custom routes.

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -2,12 +2,20 @@
 
 namespace ExploreCalifornia.Controllers
 {
+    [Route("blog")]
     public class BlogController : Controller
     {
-        // GET
+        [Route("")]
         public IActionResult Index()
         {
             return new ContentResult { Content = "Blog post." };
+        }
+
+        [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
+        public IActionResult Post(int year, int month, string key)
+        {
+            var content = $"Seeing the blog post named \"{key}\" from {year}/{month}";
+            return new ContentResult { Content = content };
         }
     }
 }
