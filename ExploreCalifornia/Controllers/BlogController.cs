ae42b4247ba0404e2b998c9eaa853118ecf6dcaf
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-16 12:04:24 +0100

    Created a layout to specify the common elements of the website layout. Blog controller actions now render Razor pages.

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -8,14 +8,13 @@ namespace ExploreCalifornia.Controllers
         [Route("")]
         public IActionResult Index()
         {
-            return new ContentResult { Content = "Blog post." };
+            return View();
         }
 
         [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year, int month, string key)
         {
-            var content = $"Seeing the blog post named \"{key}\" from {year}/{month}";
-            return new ContentResult { Content = content };
+            return View();
         }
     }
 }
