52ca9c648f0a44fa2d3d8984090cc03a6bc121df
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-17 07:19:00 +0100

    We now use tag helpers to render our form.

--- ExploreCalifornia/Models/Blog/Post.cs
+++ ExploreCalifornia/Models/Blog/Post.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ExploreCalifornia.Models.Blog
 {
     public class Post
     {
+        [Required]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "The length of the title must be between 5 and 100 characters long.")]
         public string Title { get; set; }
+
         public DateTime Posted { get; set; }
+
         public string Author { get; set; }
+
+        [Required]
+        [MinLength(100, ErrorMessage = "ananke")]
         public string Body { get; set; }
     }
 }
