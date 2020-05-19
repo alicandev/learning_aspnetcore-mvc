98c29300a12c4b855dddb40b0c27990f6eaae0e4
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-19 11:25:17 +0100

    MonthlySpecialsViewComponent implemented and is used in place of the static html content for monthly specials. Only returns a string with a TODO text at the moment.

---
+++ ExploreCalifornia/ViewComponents/MonthlySpecialsViewComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace ExploreCalifornia.ViewComponents
+{
+    public class MonthlySpecialsViewComponent
+    {
+        public string Invoke()
+        {
+            return "TODO: Show monthly specials";
+        }
+    }
+}
