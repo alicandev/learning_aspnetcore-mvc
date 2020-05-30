3e3fc99fd6454fe87527ed2be3908b76d76d0cae
= <62238518+demirtas-dev@users.noreply.github.com>
2020-05-30 18:00:01 +0100

    IdentityServices configured.

---
+++ ExploreCalifornia/IdentityDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace ExploreCalifornia
+{
+    public class IdentityDataContext : IdentityDbContext
+    {
+        public IdentityDataContext(DbContextOptions<IdentityDataContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        
+    }
+}
--- ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/Startup.cs
@@ -7,6 +7,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,36 +29,49 @@ namespace ExploreCalifornia
         
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BlogDataContext>(options => 
+                options.UseSqlServer(_configuration.GetConnectionString("BlogDataContext"))
+            );
+
+            services.AddDbContext<IdentityDataContext>(options =>
+                options.UseSqlServer(_configuration.GetConnectionString("IdentityDataContext"))
+            );
+
             services.AddTransient<FormattingServices>();
             
             services.AddTransient(x => new FeatureToggles {
                 DeveloperExceptions = _configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions")
             });
-            
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityDataContext>();
+
             services.AddMvc(options => options.EnableEndpointRouting = false);
             
-            services.AddDbContext<BlogDataContext>(options => 
-                options.UseSqlServer(_configuration.GetConnectionString("BlogDataContext"))
-            );
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, FeatureToggles features)
         {
-            if (features.DeveloperExceptions) app.UseDeveloperExceptionPage();
-            else app.UseExceptionHandler("/error.html");
-
+            if (features.DeveloperExceptions) 
+                app.UseDeveloperExceptionPage();
+            else 
+                app.UseExceptionHandler("/error.html");
             app.Use(async (ctx, next) =>
             {
                 if (ctx.Request.Path.Value.Contains("invalid")) throw new Exception("Woah!");
                 await next();
             });
-
+            
+            app.UseFileServer();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            
             app.UseMvc(routes =>
             {
                 routes.MapRoute("Default", 
                     "{controller=Home}/{action=Index}/{id?}");
             });
-            app.UseFileServer();
+            
         }
     }
 }
